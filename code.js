b34be68f90affff1a4156f5b66fdbc9dc2fc50d8
Luke Miles <rashreportloaf@gmail.com>
2018-03-03 15:44:32 -0500

    added wasd/ijkl movement and rotation

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+var KC = {'LA':37,'UA':38,'RA':39,'DA':40,'0':48,'1':49,'2':50,'3':51,'4':52,'5':53,'6':54,'7':55,'8':56,'9':57,'A':65,'B':66,'C':67,'D':68,'E':69,'F':70,'G':71,'H':72,'I':73,'J':74,'K':75,'L':76,'M':77,'N':78,'O':79,'P':80,'Q':81,'R':82,'S':83,'T':84,'U':85,'V':86,'W':87,'X':88,'Y':89,'Z':90}; // keycodes
 var camera, scene, renderer;
 var geometry, material, mesh;
 
@@ -27,9 +28,33 @@ function animate() {
 
   requestAnimationFrame( animate );
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+  //mesh.rotation.x += 0.01;
+  //mesh.rotation.y += 0.02;
 
   renderer.render( scene, camera );
 
 }
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == KC.W) {
+        mesh.position.y += .01;
+    } else if (keyCode == KC.S) {
+        mesh.position.y -= .01;
+    } else if (keyCode == KC.A) {
+        mesh.position.x -= .01;
+    } else if (keyCode == KC.D) {
+        mesh.position.x += .01;
+    } else if (keyCode == KC.I) {
+        mesh.rotation.x += .1;
+    } else if (keyCode == KC.K) {
+        mesh.rotation.x -= .1;
+    } else if (keyCode == KC.J) {
+        mesh.rotation.y += .1;
+    } else if (keyCode == KC.L) {
+        mesh.rotation.y -= .1;
+    } else if (keyCode == 32) {
+        mesh.position.set(0, 0, 0);
+    }
+};
