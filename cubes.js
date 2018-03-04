5f65f2e746579dce9b9000902edcd39977bcba1a
Luke Miles <rashreportloaf@gmail.com>
2018-03-04 06:14:54 -0500

    added lean in for speed boost

--- cubes.js
+++ cubes.js
@@ -3,12 +3,11 @@
 // Authors: Robert Max Williams and Luke Harold Miles. Public Domain Dedication.
 
 (function() {
-var camera, scene, renderer;
-var cube, sphere, light, texture, speed=-.1;
+var camera, scene, renderer; // primary objects
+var cube, sphere, light, texture; // secondary objects
 
-var rotationSpeed = 0.001; // speed of rotation
-init();
-animate();
+init(); // load all the objects into the scene
+animate(); // move them around
 
 function init() {
   texture = new THREE.TextureLoader().load('texture.png'); // inner surface of sphere
@@ -43,13 +42,18 @@ function init() {
 }
 
 function animate() {
-  var point = faces[0].points[62]; // tip of the nose
-  var [xdiff, ydiff] = [point.x-x0, point.y-y0]; // distance between current coords and initial coords
-  requestAnimationFrame(animate); // rerun on next frame
+  var leftTemple  = faces[0].points[0];
+  var rightTemple = faces[0].points[14];
+  var faceWidth = Math.sqrt(Math.pow(leftTemple.x-rightTemple.x,2) + Math.pow(leftTemple.y-rightTemple.y,2));
+  var speed = Math.pow(faceWidth/150,2)/10;
 
-  cube.translateZ(speed); // cube goes forward in direction facing
+  cube.translateZ(-speed); // cube goes forward in direction facing
   if (cube.position.length() > 100) // cube is outside sphere
     cube.position.setLength(95); // bring it back in
+
+  var rotationSpeed = .001;
+  var nose = faces[0].points[62]; // tip of the nose
+  var [xdiff, ydiff] = [nose.x-x0, nose.y-y0]; // distance between current coords and initial coords
 
   if (Math.abs(ydiff) > 20)
     cube.rotation.x += ysign * Math.sign(ydiff)*(Math.abs(ydiff)-20) * rotationSpeed; // cube turns with face
@@ -60,7 +64,7 @@ function animate() {
   [camera.rotation.x, camera.rotation.y, camera.rotation.z] =
     [cube.rotation.x, cube.rotation.y, cube.rotation.z]; // the camera looks in the same direction
 
-  //camera.lookAt(cube.position);
   renderer.render(scene, camera); // update the scene
+  requestAnimationFrame(animate); // rerun on next frame
 }
 })();
